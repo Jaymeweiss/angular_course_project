ef383b6ba79d74faf007a9e5394dfa7a4a677fd5
jayme.weiss <jayme.weiss@entelect.co.za>
2020-11-17 08:32:40 +0200

    implement non routing navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  constructor() { }
+
+  constructor() {
+  }
+
+  @Output() featureSelected = new EventEmitter<string>();
 
   ngOnInit(): void {
+  }
+
+  onSelect(feature: string): void {
+    this.featureSelected.emit(feature);
   }
 
 }
