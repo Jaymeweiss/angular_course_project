66fe550996b1ca37253df94e7c9ac669415e9a48
jayme.weiss <jayme.weiss@entelect.co.za>
2020-11-17 17:36:58 +0200

    add shopping list service

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,20 +1,38 @@
-import {EventEmitter, Injectable, OnInit} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is simply a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
-    new Recipe('Another recipe', 'This is simply another test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg')
+    new Recipe('Tasty Pasta',
+      'Tasty Pasta with a creamy sauce',
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg',
+      [
+        new Ingredient('pasta', 1),
+        new Ingredient('basil', 2)
+      ]
+    ),
+    new Recipe('Burger',
+      'A great tasting burger',
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg',
+      [
+        new Ingredient('Bun', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
   ];
   recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() {
+  constructor(private shoppingListService: ShoppingListService) {
   }
 
   getRecipes(): Recipe[] {
     return this.recipes.slice(); // return a duplicate not the pointer
   }
 
-
+  addIngredientsToShoppingList(ingredients: Ingredient[]): void {
+    this.shoppingListService.addIngredients(ingredients);
+  }
 }
