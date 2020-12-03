0d91aaf4a7e6a1e81c450cb2f9efb7aeeebdcafe
Jaymeweiss <jaymeweiss@gmail.com>
2020-12-03 13:34:02 +0200

    add update functionality for shopping list items

--- src/app/shopping-list/shopping-list-editor/shopping-list-editor.component.ts
+++ src/app/shopping-list/shopping-list-editor/shopping-list-editor.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
 import {NgForm} from '@angular/forms';
@@ -10,9 +10,11 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./shopping-list-editor.component.css']
 })
 export class ShoppingListEditorComponent implements OnInit, OnDestroy {
+  @ViewChild('form', {static: false}) shoppingListForm: NgForm;
   startedEditingSubscription: Subscription;
   editMode = false;
   editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private shoppingListService: ShoppingListService) {
   }
@@ -26,11 +28,23 @@ export class ShoppingListEditorComponent implements OnInit, OnDestroy {
       (index: number) => {
         this.editMode = true;
         this.editedItemIndex = index;
+        this.editedItem = this.shoppingListService.getIngredient(index);
+        this.shoppingListForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
       });
   }
 
   onAddItem(form: NgForm): void {
     const value = form.value;
-    this.shoppingListService.addIngredient(new Ingredient(value.name, value.amount));
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
+    form.reset();
+    this.editMode = false;
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -16,6 +16,10 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number): Ingredient {
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient): void {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
@@ -24,6 +28,11 @@ export class ShoppingListService {
   addIngredients(ingredients: Ingredient[]): void {
     this.ingredients.push(...ingredients);
     console.log('hit');
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient): void {
+    this.ingredients[index] = newIngredient;
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
