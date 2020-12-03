04ecf6e1b2bba37aa0341ef0c216d0181362aa73
Jaymeweiss <jaymeweiss@gmail.com>
2020-12-03 13:09:51 +0200

    add edit subject

--- src/app/shopping-list/shopping-list-editor/shopping-list-editor.component.ts
+++ src/app/shopping-list/shopping-list-editor/shopping-list-editor.component.ts
@@ -1,18 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
 import {NgForm} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list-editor',
   templateUrl: './shopping-list-editor.component.html',
   styleUrls: ['./shopping-list-editor.component.css']
 })
-export class ShoppingListEditorComponent implements OnInit {
+export class ShoppingListEditorComponent implements OnInit, OnDestroy {
+  startedEditingSubscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+
   constructor(private shoppingListService: ShoppingListService) {
   }
 
+  ngOnDestroy(): void {
+    this.startedEditingSubscription.unsubscribe();
+  }
+
   ngOnInit(): void {
+    this.startedEditingSubscription = this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+      });
   }
 
   onAddItem(form: NgForm): void {
