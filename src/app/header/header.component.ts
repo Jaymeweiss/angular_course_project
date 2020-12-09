2abb19d09066a490da70a123e0c7625669e4fe6e
Jaymeweiss <jaymeweiss@gmail.com>
2020-12-09 12:17:43 +0200

    update header content for logged in user

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {RecipeService} from '../recipe-book/recipe.service';
+import {AuthService} from '../auth/auth.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   collapsed = true;
+  userSubscription: Subscription;
+  isAuthenticated = false;
 
-  constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService, private authService: AuthService) {
   }
 
   ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user; // a double negative to do a presence check but not assign it to the actual value
+    });
   }
 
   onSaveData(): void {
@@ -21,5 +28,9 @@ export class HeaderComponent implements OnInit {
 
   onFetchData(): void {
     this.recipeService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 }
